60fdde3c8be445f4d8d1ddb421d0e83252cab1a5
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-20 15:17:13 +0530

    [Amit]Add : Reflection Class Room Study and Examples

---
+++ Reflection_Demo/Reflection_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Reflection_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Reflection_Problem.DataInfo();
+        }
+    }
+}
---
+++ Reflection_Demo/Reflection_Demo/Reflection_Problem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_Demo
+{
+    //Reflection allows to inspect for information of classs,method,Constctor field at run time.
+    //It is a pross of descriving the meta data of a method,constructor fiels etc in a code.
+    //Meta data refers ta a binary information saved in a memory.
+    public class Reflection_Problem
+    {
+        public static void DataInfo()
+        {
+            Type type = Type.GetType("Reflection_Demo.CustomerClass");
+            Console.WriteLine(type.FullName);
+            Console.WriteLine(type.Name);
+            Console.WriteLine(type.Namespace);
+            Console.WriteLine("****************************************");
+            PropertyInfo[] property = type.GetProperties();
+            foreach (PropertyInfo info in property)
+            {
+                Console.WriteLine(info.PropertyType.Name+":"+info.Name);
+            }
+            Console.WriteLine("******************************************");
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+            Console.WriteLine("*******************************************");
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo info in methods)
+            {
+                Console.WriteLine(info.ReturnType.Name+":"+info.Name);
+            }
+        }
+    }
+}
